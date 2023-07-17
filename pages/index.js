c614a731d3b0eac1f988e2dcbc8439b49c75149a
Douglas Espíndola <48419906+douglas-espindola@users.noreply.github.com>
2023-07-17 02:48:16 +0000

    adicionado pasta pages

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home()
+{
+    return <h1>Oi, bonita!❤️ Eu amo você 🌹 Você é uma companheira incrível, eu tenho muito orgulho de você, e estou mega feliz com o que já construímos e estamos construíndo para o nosso futuro! 😘</h1>
+}
+
+export default Home;
