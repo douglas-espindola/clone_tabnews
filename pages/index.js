3969a879159c5b27b576913bf15b5357f29f2be1
Douglas Espíndola <48419906+douglas-espindola@users.noreply.github.com>
2023-07-18 22:26:59 +0000

    alterado frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home()
 {
-    return <h1>Oi, bonita!❤️ Eu amo você 🌹 Você é uma companheira incrível, eu tenho muito orgulho de você, e estou mega feliz com o que já construímos e estamos construíndo para o nosso futuro! 😘</h1>
+    return <h1>Oi, bonita!❤️ Eu amo você 🌹 Você é uma companheira incrível, eu tenho muito orgulho de você, e estou mega feliz com o que já construímos e estamos construíndo para o nosso futuro! 😘 <br/><br/><br/> Se você me ama, dá risadinha! 😎 </h1>
+
 }
 
 export default Home;
